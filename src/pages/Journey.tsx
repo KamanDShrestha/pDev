74f60a1c019d1bacce149c338301e86ec1b009f3
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-11-03 12:42:15 +0545

    add: added icon images for journey and started Journey Card

--- src/pages/Journey.tsx
+++ src/pages/Journey.tsx
@@ -1,3 +1,13 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../components/ui/card';
+
+import stoicismIcon from '../assets/journeyIcons/Stoicism/stoicismLight.jpeg';
+
 const Journey = () => {
   return (
     <div className='p-5 mt-5 mb-5'>
@@ -6,6 +16,37 @@ const Journey = () => {
       </div>
       <div className='mt-5'>
         <h2 className='text-4xl font-semibold'>Journeys</h2>
+        <div>
+          <Card className='w-[350px]'>
+            <CardHeader>
+              <CardTitle>
+                <div className='flex items-center gap-10'>
+                  Stoicism
+                  <img src={stoicismIcon} className='w-32' />
+                </div>
+              </CardTitle>
+              <CardDescription>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam voluptatem, iusto, fugiat, voluptatum voluptates quas
+                voluptatibus quia doloribus molestias quos quibusdam
+                exercitationem. Quisquam voluptatem, iusto, fugiat, voluptatum
+                voluptates quas voluptatibus quia doloribus molestias quos
+                quibusdam exercitationem.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div>
+                <h4>Importance</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quisquam voluptatem, iusto, fugiat, voluptatum voluptates quas
+                  voluptatibus quia doloribus molestias quos quibusdam
+                  exercitationem.
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   );
