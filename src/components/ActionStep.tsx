d14691fe5ce06537b40f1f0e6a8e96cd5e1d6dbb
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-11-04 21:45:01 +0545

    add: added ActionStep component

---
+++ src/components/ActionStep.tsx
@@ -0,0 +1,111 @@
+import { AiTwotoneUpCircle } from 'react-icons/ai';
+import InputFieldLabel from './InputFieldLabel';
+import { Input } from './ui/input';
+import ErrorMessage from './ErrorMessage';
+import {
+  FieldErrors,
+  FieldValues,
+  UseFormRegister,
+  UseFormWatch,
+} from 'react-hook-form';
+import { Textarea } from './ui/textarea';
+
+interface ActionStepProps {
+  day: number;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
+  watch: UseFormWatch<FieldValues>;
+}
+
+const ActionStep = ({ day, register, errors, watch }: ActionStepProps) => {
+  const providedActionStep = watch(`day${day}actionStep`);
+  const providedDescription = watch(`day${day}description`);
+  console.log(day, providedActionStep, providedDescription);
+
+  const providedAdditionalActionStep1 = watch(`day${day}additionalStep1`);
+  const providedAdditionalActionStep2 = watch(`day${day}additionalStep2`);
+  const providedAdditionalActionStep3 = watch(`day${day}additionalStep3`);
+  console.log(
+    providedAdditionalActionStep1,
+    providedAdditionalActionStep2,
+    providedAdditionalActionStep3
+  );
+  return (
+    <div className='flex flex-col gap-3'>
+      <div className='flex items-center gap-4'>
+        <AiTwotoneUpCircle />
+        <h2 className='text-xl font-semibold'>Day {day}</h2>
+      </div>
+      <div className='flex flex-col gap-4'>
+        <div className='relative group'>
+          <InputFieldLabel
+            htmlFor={`day${day}actionStep`}
+            hasContent={
+              providedActionStep !== undefined &&
+              providedActionStep?.length !== 0
+            }
+          >
+            Action Step
+          </InputFieldLabel>
+          <Input {...register(`day${day}actionStep`)} type='text' />
+          {errors[`day${day}actionStep`] && (
+            <ErrorMessage>{errors.journeyName?.message as string}</ErrorMessage>
+          )}
+        </div>
+
+        <div className='relative group'>
+          <InputFieldLabel
+            htmlFor={`day${day}description`}
+            hasContent={
+              providedDescription !== undefined &&
+              providedDescription?.length !== 0
+            }
+          >
+            Description
+          </InputFieldLabel>
+          <Textarea {...register(`day${day}description`)} />
+          {errors[`day${day}description`] && (
+            <ErrorMessage>{errors.journeyName?.message as string}</ErrorMessage>
+          )}
+        </div>
+        <div className='relative flex flex-col gap-2 group'>
+          <label htmlFor={`${day}additionalActionSteps`}>
+            Additional Steps
+          </label>
+
+          {Array.from(Array(3)).map((_, index) => (
+            <>
+              <Input
+                key={index}
+                {...register(`day${day}additionalStep${index + 1}`)}
+              />
+            </>
+          ))}
+          {/* {errors.journeyName && (
+            <ErrorMessage>{errors.journeyName?.message as string}</ErrorMessage>
+          )} */}
+        </div>
+
+        <div className='relative flex flex-col gap-2 group'>
+          <label htmlFor={`${day}additionalActionSteps`}>
+            Evidences and further Learnings
+          </label>
+
+          {Array.from(Array(3)).map((_, index) => (
+            <>
+              <Input
+                key={index}
+                {...register(`day${day}evidence${index + 1}`)}
+              />
+            </>
+          ))}
+          {/* {errors.journeyName && (
+            <ErrorMessage>{errors.journeyName?.message as string}</ErrorMessage>
+          )} */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActionStep;
--- src/pages/admin/AddJourneyPage.tsx
+++ src/pages/admin/AddJourneyPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { FieldValue, FieldValues, useForm } from 'react-hook-form';
 import * as zod from 'zod';
 import {
   Card,
@@ -12,12 +12,15 @@ import {
 import InputFieldLabel from '../../components/InputFieldLabel';
 import { Input } from '../../components/ui/input';
 import ErrorMessage from '../../components/ErrorMessage';
+import { Button } from '../../components/ui/button';
+import ActionStep from '../../components/ActionStep';
 const AddJourneyPage = () => {
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
+    getValues,
   } = useForm();
   const [numberOfActionSteps, setNumberOfActionSteps] = useState(10);
 
@@ -27,13 +30,17 @@ const AddJourneyPage = () => {
     console.log('clicked');
   }
 
+  function handleSaveButton() {
+    console.log(getValues());
+  }
+
   return (
     <div className='flex flex-col items-center justify-center w-screen min-h-screen gap-10 overflow-scroll'>
       <div className='w-[350px] sm:w-[400px] h-[200px] bg-slate-100'>
         Preview
       </div>
       <div className='flex flex-wrap items-center justify-around gap-5'>
-        <Card>
+        <Card className='overflow-scroll'>
           <CardHeader>
             <CardTitle>Add new journey for the users !</CardTitle>
             <CardDescription>
@@ -42,85 +49,88 @@ const AddJourneyPage = () => {
           </CardHeader>
           <CardContent>
             <form onSubmit={handleSubmit(handleJourneySubmit)}>
-              <div className='relative group'>
-                <InputFieldLabel
-                  htmlFor='journeyName'
-                  hasContent={
-                    providedName !== undefined && providedName?.length !== 0
-                  }
-                >
-                  Journey Name
-                </InputFieldLabel>
-                <Input {...register('journeyName')} type='text' />
-                {errors.journeyName && (
-                  <ErrorMessage>
-                    {errors.journeyName?.message as string}
-                  </ErrorMessage>
-                )}
-              </div>
-              <div className='relative group'>
-                <InputFieldLabel
-                  htmlFor='journeyDescription'
-                  hasContent={
-                    providedName !== undefined && providedName?.length !== 0
-                  }
-                >
-                  Journey Description
-                </InputFieldLabel>
-                <Input {...register('journeyDescription')} type='text' />
-                {errors.journeyDescription && (
-                  <ErrorMessage>
-                    {errors.journeyDescription.message as string}
-                  </ErrorMessage>
-                )}
-              </div>
-              <div className='relative group'>
-                <InputFieldLabel
-                  htmlFor='journeyImportance'
-                  hasContent={
-                    providedName !== undefined && providedName?.length !== 0
-                  }
-                >
-                  Journey Importance
-                </InputFieldLabel>
-                <Input {...register('journeyImportance')} type='text' />
-                {errors.journeyImportance && (
-                  <ErrorMessage>
-                    {errors.journeyImportance.message as string}
-                  </ErrorMessage>
-                )}
-              </div>
-              <div className='relative group'>
-                <InputFieldLabel
-                  htmlFor='journeyUsages'
-                  hasContent={
-                    providedName !== undefined && providedName?.length !== 0
-                  }
-                >
-                  Journey Usages
-                </InputFieldLabel>
-                <Input {...register('journeyUsages')} type='text' />
-                {errors.journeyUsages && (
-                  <ErrorMessage>
-                    {errors.journeyUsages.message as string}
-                  </ErrorMessage>
-                )}
-              </div>
-              <div className='relative group'>
-                <InputFieldLabel
-                  htmlFor='journeyQuotes'
-                  hasContent={
-                    providedName !== undefined && providedName?.length !== 0
-                  }
-                >
-                  Journey Quotes
-                </InputFieldLabel>
-                <Input {...register('journeyQuotes')} type='text' />
-                {errors.journeyQuotes && (
-                  <ErrorMessage>
-                    {errors.journeyQuotes.message as string}
-                  </ErrorMessage>
-                )}
+              <div className='flex flex-col gap-3'>
+                <div className='relative group'>
+                  <InputFieldLabel
+                    htmlFor='journeyName'
+                    hasContent={
+                      providedName !== undefined && providedName?.length !== 0
+                    }
+                  >
+                    Journey Name
+                  </InputFieldLabel>
+                  <Input {...register('journeyName')} type='text' />
+                  {errors.journeyName && (
+                    <ErrorMessage>
+                      {errors.journeyName?.message as string}
+                    </ErrorMessage>
+                  )}
+                </div>
+                <div className='relative group'>
+                  <InputFieldLabel
+                    htmlFor='journeyDescription'
+                    hasContent={
+                      providedName !== undefined && providedName?.length !== 0
+                    }
+                  >
+                    Journey Description
+                  </InputFieldLabel>
+                  <Input {...register('journeyDescription')} type='text' />
+                  {errors.journeyDescription && (
+                    <ErrorMessage>
+                      {errors.journeyDescription.message as string}
+                    </ErrorMessage>
+                  )}
+                </div>
+                <div className='relative group'>
+                  <InputFieldLabel
+                    htmlFor='journeyImportance'
+                    hasContent={
+                      providedName !== undefined && providedName?.length !== 0
+                    }
+                  >
+                    Journey Importance
+                  </InputFieldLabel>
+                  <Input {...register('journeyImportance')} type='text' />
+                  {errors.journeyImportance && (
+                    <ErrorMessage>
+                      {errors.journeyImportance.message as string}
+                    </ErrorMessage>
+                  )}
+                </div>
+                <div className='relative group'>
+                  <InputFieldLabel
+                    htmlFor='journeyUsages'
+                    hasContent={
+                      providedName !== undefined && providedName?.length !== 0
+                    }
+                  >
+                    Journey Usages
+                  </InputFieldLabel>
+                  <Input {...register('journeyUsages')} type='text' />
+                  {errors.journeyUsages && (
+                    <ErrorMessage>
+                      {errors.journeyUsages.message as string}
+                    </ErrorMessage>
+                  )}
+                </div>
+                <div className='relative group'>
+                  <InputFieldLabel
+                    htmlFor='journeyQuotes'
+                    hasContent={
+                      providedName !== undefined && providedName?.length !== 0
+                    }
+                  >
+                    Journey Quotes
+                  </InputFieldLabel>
+                  <Input {...register('journeyQuotes')} type='text' />
+                  {errors.journeyQuotes && (
+                    <ErrorMessage>
+                      {errors.journeyQuotes.message as string}
+                    </ErrorMessage>
+                  )}
+                </div>
+                <Button>Submit</Button>
               </div>
             </form>
           </CardContent>
@@ -130,11 +140,21 @@ const AddJourneyPage = () => {
           <CardHeader>
             <CardTitle>Add action steps for the journey</CardTitle>
           </CardHeader>
-          <CardContent>
+          <CardContent className='flex flex-col gap-10 h-[550px] overflow-scroll md:w-[640px]'>
             {Array.from(Array(numberOfActionSteps)).map((_, index) => (
-              <p>{index + 1}</p>
+              <>
+                <ActionStep
+                  day={index + 1}
+                  register={register}
+                  errors={errors}
+                  watch={watch}
+                />
+              </>
             ))}
           </CardContent>
+          <CardFooter>
+            <Button onClick={handleSaveButton}>Save</Button>
+          </CardFooter>
         </Card>
       </div>
     </div>
