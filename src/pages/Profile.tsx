a57a5cf36a16ca9079547e0b1d15bcd4e3f0f33d
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-11-22 06:47:33 +0545

    add: initial setup for Profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import { useAuthContext } from '../context/AuthProvider';
+import Heading from '../components/Heading';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { user } = useAuthContext();
+  return (
+    <>
+      <div className='flex flex-wrap items-center justify-between gap-5'>
+        <div>
+          <span style={{ fontSize: '200px' }}>👦🏻</span>
+        </div>
+        <div>
+          <Heading>Personal Details</Heading>
+          <div>
+            <div>
+              <span>Name: </span>
+              <span>{user?.firstName}</span>
+            </div>
+            <div>
+              <span>Email: </span>
+              <span>{user?.email}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
