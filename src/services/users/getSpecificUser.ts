ecdcfff95184744c5e504646617d6b5c05debb5d
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-12-02 07:32:14 +0545

    add: added function to get specific user

--- src/services/users/getSpecificUser.ts
+++ src/services/users/getSpecificUser.ts
@@ -1,11 +1,11 @@
-// import { axiosInstance } from '../../constants';
-// import { useQuery } from '@tanstack/react-query';
+import { axiosInstance } from '../../constants';
+import { useQuery } from '@tanstack/react-query';
 
-// export default function useGetSpecificUser(userId: string) {
-//   const response = useQuery({
-//     queryKey: ['user', userId],
-//     queryFn: () =>
-//       axiosInstance.get(`/users/get/${userId}`).then((res) => res.data.user),
-//   });
-//   return response;
-// }
+export default function useGetSpecificUser(userId: string) {
+  const response = useQuery({
+    queryKey: ['user', userId],
+    queryFn: () =>
+      axiosInstance.get(`/users/get/${userId}`).then((res) => res.data.user),
+  });
+  return response;
+}
