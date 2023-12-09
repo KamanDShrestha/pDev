7d7b93b4444087703d786992a76e696a48a2ae1f
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-12-09 17:20:08 +0545

    add: added success and error toast for addition of feedbacks

--- src/services/journeyFeedbacks/addJourneyFeedback.ts
+++ src/services/journeyFeedbacks/addJourneyFeedback.ts
@@ -1,11 +1,19 @@
+import toast from 'react-hot-toast';
 import { axiosInstance } from '../../constants';
-import { AddJourneyFeedbackData } from '../../types';
+import { AddJourneyFeedbackData, ErrorResponse } from '../../types';
 import { useMutation } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 export default function useAddJourneyFeedback() {
   const response = useMutation({
     mutationFn: (data: AddJourneyFeedbackData) =>
       axiosInstance.post('/journeyFeedbacks/add', data).then((res) => res.data),
+    onSuccess: (response) => {
+      toast.success(response.message);
+    },
+    onError: (error: AxiosError<ErrorResponse>) => {
+      toast.error(error.response?.data.message || 'An error occured');
+    },
   });
   return response;
 }
