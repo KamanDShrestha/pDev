73d74bdddf45690531e424b80b8bff6e95627565
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-12-09 18:55:22 +0545

    add: added feedbacks to render feedback based on status along with Act button

--- src/pages/admin/Feedbacks.tsx
+++ src/pages/admin/Feedbacks.tsx
@@ -10,12 +10,59 @@ const Feedbacks = () => {
       <div>
         <Heading>Feedbacks for journeys</Heading>
 
-        {isLoading && <div>Loading...</div>}
+        <div className='flex flex-wrap justify-center gap-4'>
+          {isLoading && <div>Loading...</div>}
 
-        {journeyFeedbacks &&
-          journeyFeedbacks?.map((feedback, index) => (
-            <JourneyFeedbackCard feedback={feedback} key={index} />
-          ))}
+          {journeyFeedbacks &&
+          journeyFeedbacks.filter(
+            (feedback) => feedback.feedbackStatus === 'pending'
+          ).length !== 0 ? (
+            journeyFeedbacks
+              .filter((feedback) => feedback.feedbackStatus === 'pending')
+              .map((feedback, index) => (
+                <JourneyFeedbackCard feedback={feedback} key={index} />
+              ))
+          ) : (
+            <div>No pending feedbacks</div>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <Heading>Resolved Feedbacks</Heading>
+        {isLoading && <div>Loading...</div>}
+        <div className='flex flex-wrap justify-center gap-4'>
+          {journeyFeedbacks &&
+          journeyFeedbacks.filter(
+            (feedback) => feedback.feedbackStatus === 'resolved'
+          ).length !== 0 ? (
+            journeyFeedbacks
+              .filter((feedback) => feedback.feedbackStatus === 'resolved')
+              .map((feedback, index) => (
+                <JourneyFeedbackCard feedback={feedback} key={index} />
+              ))
+          ) : (
+            <div>No rejected feedbacks</div>
+          )}
+        </div>
+      </div>
+      <div>
+        <Heading>Rejected Feedbacks</Heading>
+        {isLoading && <div>Loading...</div>}
+        <div className='flex flex-wrap justify-center gap-4'>
+          {journeyFeedbacks &&
+          journeyFeedbacks.filter(
+            (feedback) => feedback.feedbackStatus === 'rejected'
+          ).length !== 0 ? (
+            journeyFeedbacks
+              .filter((feedback) => feedback.feedbackStatus === 'rejected')
+              .map((feedback, index) => (
+                <JourneyFeedbackCard feedback={feedback} key={index} />
+              ))
+          ) : (
+            <div>No rejected feedbacks</div>
+          )}
+        </div>
       </div>
     </>
   );
