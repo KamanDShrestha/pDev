b9f652163f413f9bc08cdcb5da5a7b3ebfa1a99c
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-12-16 21:26:54 +0545

    add: added functionality to get posts of users of certain community

--- src/pages/SpecificCommunity.tsx
+++ src/pages/SpecificCommunity.tsx
@@ -46,7 +46,7 @@ const SpecificCommunity = () => {
               <span style={{ fontSize: '20px' }}>
                 <BsSignpostSplit />
               </span>
-              <span className='hover:underline hover:cursor-pointer'>
+              <span className='hover:underline hover:cursor-pointer' onClick={}>
                 My posts
               </span>
             </div>
---
+++ src/services/posts/getPostsByUser.ts
@@ -0,0 +1,14 @@
+import { axiosInstance } from '../../constants';
+import { PostData } from '../../types';
+import { useQuery } from '@tanstack/react-query';
+
+export default function useGetPostsByUser(communityId: string, userId: string) {
+  const response = useQuery<PostData[]>({
+    queryKey: ['posts', communityId, userId],
+    queryFn: () =>
+      axiosInstance
+        .post(`/posts/${communityId}/${userId}`)
+        .then((res) => res.data.data),
+  });
+  return response;
+}
