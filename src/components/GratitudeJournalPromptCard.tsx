a783978e11719602c6a743b67372046caf84f329
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-01-26 18:45:28 +0545

    add: added interface for adding new prompts

---
+++ src/components/GratitudeJournalPromptCard.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+
+const GratitudeJournalPromptCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Gratitude journal prompts</CardTitle>
+      </CardHeader>
+      <CardContent></CardContent>
+    </Card>
+  );
+};
+
+export default GratitudeJournalPromptCard;
--- src/pages/admin/WellbeingForAdmin.tsx
+++ src/pages/admin/WellbeingForAdmin.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../../components/ui/card';
+import GratitudeJournalPromptCard from '../../components/GratitudeJournalPromptCard';
+import Heading from '../../components/Heading';
+import { Input } from '../../components/ui/input';
 
 const WellbeingForAdmin = () => {
-  return <div>WellbeingForAdmin</div>;
+  return (
+    <>
+      <div>
+        <Heading>Gratitude Journal</Heading>
+        <GratitudeJournalPromptCard />
+        <Card>
+          <CardHeader>
+            <CardTitle>Add new prompts here.</CardTitle>
+            <CardDescription>
+              You can add new prompts from here.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <div>
+                <Heading className='mb-0 text-lg'>Prompt</Heading>
+                <Input />
+              </div>
+              <div>
+                <Heading className='mb-0 text-lg'>Placeholder</Heading>
+                <Input />
+              </div>
+              <div>
+                <Heading className='mb-0 text-lg'>Category</Heading>
+                <Input />
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default WellbeingForAdmin;
