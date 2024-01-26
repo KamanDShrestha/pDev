905390bb4c354a24f45edde1d853f3a2739dbdf6
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-01-26 12:55:57 +0545

    add: added dialog for providing all the content of journal

--- src/components/JournalCard.tsx
+++ src/components/JournalCard.tsx
@@ -3,6 +3,13 @@ import { Journal } from '../types';
 import Heading from './Heading';
 import { Badge } from './ui/badge';
 import TruncatedText from './TruncatedText';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
 
 interface JournalCardProps {
   journal: Journal;
@@ -10,20 +17,40 @@ interface JournalCardProps {
 
 const JournalCard = ({ journal }: JournalCardProps) => {
   return (
-    <Card className='lg:max-w-[600px] w-[400px] h-[300px] grid grid-rows-[20%_60%_20%]'>
-      <CardHeader className='flex items-end w-full'>
-        <Badge>{journal.journalCategory}</Badge>
-      </CardHeader>
-      <CardContent className='flex items-center'>
-        <TruncatedText content={journal.journalContent} />
-      </CardContent>
-      <CardFooter className='flex items-center justify-between'>
-        <Heading className='my-0 text-xl'>{journal.journalTitle}</Heading>
-        <div className='text-sm'>
-          {new Date(journal.entryDate).toDateString()}
-        </div>
-      </CardFooter>
-    </Card>
+    <>
+      <Dialog>
+        <DialogTrigger>
+          <Card className='lg:max-w-[600px] w-[400px] h-[300px] grid grid-rows-[20%_60%_20%]'>
+            <CardHeader className='flex items-end w-full'>
+              <Badge>{journal.journalCategory}</Badge>
+            </CardHeader>
+            <CardContent className='flex items-center'>
+              <TruncatedText content={journal.journalContent} />
+            </CardContent>
+            <CardFooter className='flex items-center justify-between'>
+              <Heading className='my-0 text-xl'>{journal.journalTitle}</Heading>
+              <div className='text-sm'>
+                {new Date(journal.entryDate).toDateString()}
+              </div>
+            </CardFooter>
+          </Card>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{journal.journalTitle}</DialogTitle>
+          </DialogHeader>
+          <div className='flex justify-between'>
+            <span className='text-sm'>
+              {new Date(journal.entryDate).toDateString()}
+            </span>
+            <Badge>{journal.journalCategory}</Badge>
+          </div>
+          <div>
+            <p>{journal.journalContent}</p>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
