b51d77290dbcf98689553f420c65e135a6931e3a
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-01-31 15:52:23 +0545

    add: added button for navigating to subscribe page for user from admin view

--- src/pages/SubscribeForAdmin.tsx
+++ src/pages/SubscribeForAdmin.tsx
@@ -1,14 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import Heading from '../components/Heading';
 import SubscriptionPlanAddSection from '../components/SubscriptionPlanAddSection';
 import SubscriptionPlanCard from '../components/SubscriptionPlanCard';
+import { Button } from '../components/ui/button';
 import useGetSubscriptionPlans from '../services/subscriptionPlans/getSubscriptionPlans';
 
 const SubscribeForAdmin = () => {
   const { data: subscriptionPlans } = useGetSubscriptionPlans();
+  const navigate = useNavigate();
   return (
     <>
-      <Heading>Subscription Plans</Heading>
-      <div>
+      <div className='flex flex-wrap justify-between'>
+        <Heading>Subscription Plans</Heading>
+        <Button variant={'link'} onClick={() => navigate('/subscribe')}>
+          User view of Subscribe page
+        </Button>
+      </div>
+
+      <div className='flex gap-5'>
         {subscriptionPlans?.length === 0 && (
           <p> No subscription plans are present currently.</p>
         )}
