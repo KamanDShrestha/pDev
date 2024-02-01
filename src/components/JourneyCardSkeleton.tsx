45792653995d9c5abefd5700c83aa30392b8d3d9
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-02-01 10:18:45 +0545

    refactor: made changes for some bugs

--- src/components/JourneyCardSkeleton.tsx
+++ src/components/JourneyCardSkeleton.tsx
@@ -1,26 +1,15 @@
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from './ui/card';
+import { Card, CardHeader, CardContent, CardFooter } from './ui/card';
 import { Skeleton } from './ui/skeleton';
 
 const JourneyCardSkeleton = () => {
   return (
     <Card className='w-[350px] sm:w-[400px] h-[500px]'>
       <CardHeader>
-        <CardTitle>
-          <div className='flex items-center justify-around gap-10'>
-            <Skeleton className='h-4 w-[100px]' />
-            <Skeleton className='w-20 h-20 rounded-full' />
-          </div>
-        </CardTitle>
-        <CardDescription>
-          <Skeleton className='h-4 w-[200px]' />
-        </CardDescription>
+        <div className='flex items-center justify-around gap-10'>
+          <Skeleton className='h-4 w-[100px]' />
+          <Skeleton className='w-20 h-20 rounded-full' />
+        </div>
+        <Skeleton className='h-4 w-[200px]' />
       </CardHeader>
       <CardContent>
         <div className='space-y-5'>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.tsx';
 import './index.css';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
@@ -9,43 +8,42 @@ import { Toaster } from 'react-hot-toast';
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <GoogleOAuthProvider clientId='948702447301-tb96h7eie0gl3hp7hfoh90drkf7dck8h.apps.googleusercontent.com'>
-      <React.StrictMode>
-        <App />
-        <Toaster
-          position='top-center'
-          reverseOrder={false}
-          gutter={8}
-          containerClassName=''
-          containerStyle={{}}
-          toastOptions={{
-            // Define default options
-            className: '',
-            duration: 5000,
-            style: {
-              background: '#363636',
-              color: '#fff',
-            },
+    <React.StrictMode>
+      <App />
+      <Toaster
+        position='top-center'
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=''
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 5000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
 
-            // Default options for specific types
-            success: {
-              duration: 3000,
-              iconTheme: {
-                primary: 'green',
-                secondary: 'black',
-              },
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: 'green',
+              secondary: 'black',
             },
-            error: {
-              duration: 3000,
-              iconTheme: {
-                primary: 'red',
-                secondary: 'black',
-              },
+          },
+          error: {
+            duration: 3000,
+            iconTheme: {
+              primary: 'red',
+              secondary: 'black',
             },
-          }}
-        />
-      </React.StrictMode>
-    </GoogleOAuthProvider>
+          },
+        }}
+      />
+    </React.StrictMode>
+
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
