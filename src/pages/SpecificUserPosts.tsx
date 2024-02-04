413af9d2b1d7704ab33ecb450d3111ce08d4ee51
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-02-04 14:46:11 +0545

    add: implemented functionality to get qas and page for displaying posts and qas of specific user

--- src/pages/SpecificUserPosts.tsx
+++ src/pages/SpecificUserPosts.tsx
@@ -5,6 +5,10 @@ import Heading from '../components/Heading';
 
 import { Button } from '../components/ui/button';
 import LoadingSpinner from '../components/LoadingSpinner';
+import useGetQAsByUserInCommunity from '../services/QAs/getQAsByUserInCommunity';
+import PostCard from '../components/PostCard';
+import useDeletePost from '../services/posts/deletePost';
+import QuestionAnswerCard from '../components/QuestionAnswerCard';
 
 const SpecificUserPosts = () => {
   const { communityId, userId } = useParams();
@@ -17,26 +21,71 @@ const SpecificUserPosts = () => {
     communityId as string,
     userId as string
   );
-  //   const {data: qas, isLoading: isQAsLoading} = useGetQ(communityId as string, userId as string);
+  const { data: qas, isLoading: isQAsLoading } = useGetQAsByUserInCommunity(
+    communityId as string,
+    userId as string
+  );
+  const { mutate: deletePost } = useDeletePost(
+    communityId as string,
+    userId as string
+  );
 
   const navigate = useNavigate();
 
   console.log(community);
+  console.log(qas);
   console.log(posts);
+
+  function handleDeletePost(postId: string) {
+    deletePost(postId);
+    console.log(postId);
+    console.log(posts);
+    posts?.filter((post) => post._id !== postId);
+  }
 
   return (
     <div>
       {(isLoading || isPostsLoading) && <LoadingSpinner />}
-      {posts && posts.length > 0 && !isPostsLoading && !isLoading ? (
-        <Heading>{`Posts by ${posts[0]?.userName} in ${community?.communityName} community`}</Heading>
-      ) : (
+      {posts && posts.length > 0 && !isPostsLoading && !isLoading && (
         <>
-          <Heading>No posts yet in this community.</Heading>
-          <Button onClick={() => navigate(-1)} variant={'link'}>
-            Go back
-          </Button>
+          <Heading>{`Posts by ${posts[0]?.userName} in ${community?.communityName} community`}</Heading>
+          <div>
+            <Heading className='text-xl'>Posts of different categories</Heading>
+            <div className='flex flex-wrap items-center justify-center gap-5 p-5'>
+              {posts.map((post, index) => (
+                <div key={index} className='w-[400px] lg:w-[600px]'>
+                  <PostCard post={post} onDeletePost={handleDeletePost} />
+                </div>
+              ))}
+            </div>
+          </div>
         </>
       )}
+      {qas && qas.length > 0 && !isQAsLoading && !isLoading && (
+        <>
+          <div>
+            <Heading className='text-xl'>{`QAs by ${qas[0]?.userName} in ${community?.communityName} community`}</Heading>
+            {qas.map((question, index) => (
+              <QuestionAnswerCard question={question} key={index} />
+            ))}
+          </div>
+        </>
+      )}
+
+      {posts &&
+        qas &&
+        posts.length === 0 &&
+        qas.length === 0 &&
+        !isPostsLoading &&
+        !isQAsLoading &&
+        !isLoading && (
+          <>
+            <Heading>No posts yet in this community.</Heading>
+            <Button onClick={() => navigate(-1)} variant={'link'}>
+              Go back
+            </Button>
+          </>
+        )}
     </div>
   );
 };
