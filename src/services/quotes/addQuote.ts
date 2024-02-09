a3407d9f35b915d2372f7547b7ad8095b8489b3d
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-02-09 12:39:57 +0545

    add: added functionality to add new quotes by admin

---
+++ src/services/quotes/addQuote.ts
@@ -0,0 +1,23 @@
+import { axiosInstance } from '@/src/constants';
+import { ErrorResponse } from '@/src/types';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import toast from 'react-hot-toast';
+
+export default function useAddQuote() {
+  const queryClient = useQueryClient();
+  const response = useMutation({
+    mutationFn: (data) =>
+      axiosInstance.post('/quotes/add', data).then((res) => res.data),
+    onSuccess: (response) => {
+      console.log(response);
+      toast.success(response.message);
+      queryClient.invalidateQueries(['quotes']);
+    },
+    onError: (error: AxiosError<ErrorResponse>) => {
+      console.log(error);
+      toast.error(error.response?.data.message || 'An error occured');
+    },
+  });
+  return response;
+}
