fb3fd373d1760f0712d5951b175c3e34cc23ed2b
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-02-09 12:54:03 +0545

    add: added functionality to get distinct quote categories

---
+++ src/services/quotes/getQuoteCategories.ts
@@ -0,0 +1,11 @@
+import { axiosInstance } from '../../constants';
+import { useQuery } from '@tanstack/react-query';
+
+export default function useGetQuoteCategories() {
+  const response = useQuery<string[]>({
+    queryKey: ['quoteCategories'],
+    queryFn: () =>
+      axiosInstance.get('/quotes/get/categories').then((res) => res.data.data),
+  });
+  return response;
+}
