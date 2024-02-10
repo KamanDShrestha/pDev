35862bdaec3fb86f89c2dc263f5bb1a009188dc9
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-02-10 13:57:36 +0545

    add: added functionality to delete learning podcast

--- src/services/learningPodcasts/deleteLearningPodcast.ts
+++ src/services/learningPodcasts/deleteLearningPodcast.ts
@@ -4,18 +4,18 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import toast from 'react-hot-toast';
 
-export default function useDeleteVideo() {
+export default function useDeletePodcast() {
   const queryClient = useQueryClient();
   const response = useMutation({
-    mutationFn: (data: { videoId: string; category: string }) =>
+    mutationFn: (data: { podcastId: string; category: string }) =>
       axiosInstance
-        .delete(`/learningVideos/delete/${data.category}/${data.videoId}`)
+        .delete(`/learningPodcasts/delete/${data.category}/${data.podcastId}`)
         .then((res) => res.data),
     onSuccess: (response) => {
       console.log(response);
       toast.success(response.message);
-      queryClient.invalidateQueries(['learningVideos']);
-      queryClient.invalidateQueries(['videoCategories']);
+      queryClient.invalidateQueries(['learningPodcasts']);
+      queryClient.invalidateQueries(['podcastCategories']);
     },
     onError: (error: AxiosError<ErrorResponse>) => {
       console.log(error);
