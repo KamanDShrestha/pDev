58880e3481185e59ff52f7fd0317f7ec8223ad8d
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-02-11 20:31:17 +0545

    add: implemented the functionality to update learning video

--- src/components/LearningVideoCard.tsx
+++ src/components/LearningVideoCard.tsx
@@ -15,6 +15,22 @@ import useGetContentSavedStatus from '../services/savedContent/getContentSavedSt
 import LoadingSpinner from './LoadingSpinner';
 import { IoBookmark, IoBookmarkOutline } from 'react-icons/io5';
 import { useQueryClient } from '@tanstack/react-query';
+import { Button, buttonVariants } from './ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
+import { FaPen } from 'react-icons/fa';
+import { cn } from '../lib/utils';
+
+import { Input } from './ui/input';
+
+import { FieldValues, useForm } from 'react-hook-form';
+import useUpdateLearningVideo from '../services/learningVideos/updateLearningVideo';
 
 interface LearningVideoCardProps {
   video: LearningVideo;
@@ -27,8 +43,19 @@ const LearningVideoCard = ({
 }: LearningVideoCardProps) => {
   const { user } = useAuthContext();
   const { mutate: saveVideo, isLoading: isSaving } = useAddSavedContent();
+  const { mutate: updateVideo, isLoading: isUpdating } =
+    useUpdateLearningVideo();
   const { data: savedContentStatus, isLoading: gettingSavedContentStatus } =
     useGetContentSavedStatus(user?.id as string, 'video', video._id);
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      title: video.title,
+      url: video.url,
+      embedUrl: video.embedUrl,
+      author: video.author,
+    },
+  });
 
   const queryClient = useQueryClient();
   function handleSavePost() {
@@ -53,6 +80,19 @@ const LearningVideoCard = ({
     );
   }
 
+  function handleVideoUpdate(data: FieldValues) {
+    updateVideo({
+      category: videoCategory,
+      videoId: video._id,
+      video: {
+        title: data.title,
+        url: data.url,
+        embedUrl: data.embedUrl,
+        author: data.author,
+      },
+    });
+  }
+
   return (
     <Card className='w-[360px]'>
       <CardHeader>
@@ -72,7 +112,7 @@ const LearningVideoCard = ({
           allowFullScreen
         ></iframe>
       </CardContent>
-      <CardFooter>
+      <CardFooter className='space-x-3'>
         <span
           className='right-0 text-xl hover:cursor-pointer'
           onClick={handleSavePost}
@@ -85,6 +125,96 @@ const LearningVideoCard = ({
             <IoBookmarkOutline />
           )}
         </span>
+        <div>
+          <Dialog>
+            <DialogTrigger
+              className={cn(
+                buttonVariants({ variant: 'default', size: 'xs' }),
+                'space-x-2'
+              )}
+            >
+              <span>Update Video</span>
+              <FaPen />
+            </DialogTrigger>
+            <DialogContent>
+              <DialogTitle>Update this video</DialogTitle>
+              <DialogDescription>
+                You can update this video here.
+              </DialogDescription>
+              <div className='space-y-2'>
+                <div>
+                  <label htmlFor='title'>Title</label>
+                  <Input
+                    id='title'
+                    {...register('title', {
+                      required: 'Title must be provided.',
+                      min: {
+                        value: 10,
+                        message: 'Title must be at least 10 characters.',
+                      },
+                    })}
+                  />
+                </div>
+                <div>
+                  <label htmlFor='url'>URL</label>
+                  <Input
+                    id='url'
+                    {...register('url', {
+                      required: 'URL must be provided.',
+                      min: {
+                        value: 10,
+                        message: 'URL must be at least 10 characters.',
+                      },
+                    })}
+                  />
+                </div>
+                <div>
+                  <label htmlFor='embedUrl'>Embed URL</label>
+                  <Input
+                    id='embedUrl'
+                    {...register('embedUrl', {
+                      required: 'Embed URL must be provided.',
+                      min: {
+                        value: 10,
+                        message: 'Embed URL must be at least 10 characters.',
+                      },
+                    })}
+                  />
+                </div>
+                <div>
+                  <label htmlFor='author'>Author</label>
+                  <Input
+                    {...register('author', {
+                      required: 'Author must be provided.',
+                      min: {
+                        value: 10,
+                        message: 'Author must be at least 10 characters.',
+                      },
+                    })}
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button
+                  className={cn(
+                    buttonVariants({ variant: 'default', size: 'xs' }),
+                    'space-x-2'
+                  )}
+                  onClick={handleSubmit(handleVideoUpdate)}
+                >
+                  {isUpdating ? (
+                    <LoadingSpinner />
+                  ) : (
+                    <>
+                      <span>Update this video</span>
+                      <FaPen />
+                    </>
+                  )}
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
       </CardFooter>
     </Card>
   );
