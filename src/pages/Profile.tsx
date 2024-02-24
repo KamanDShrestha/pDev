caa39607b8a57c322fd43f799a2655f2790ad0e1
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-02-24 15:16:28 +0545

    add: implemented functionality and user interface to render qhp details within Profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -20,6 +20,7 @@ import ProfilePersonalDetailsCard from '../components/ProfilePersonalDetailsCard
 import { Separator } from '../components/ui/separator';
 import useGetSpecificUser from '../services/users/getSpecificUser';
 import { LucideDot } from 'lucide-react';
+import useGetQhpDetails from '../services/qhpDetails/getQhpDetails';
 const Profile = () => {
   const { user } = useAuthContext();
   console.log(user);
@@ -27,6 +28,10 @@ const Profile = () => {
   const { data: userDetails, isLoading: isFetchingUserDetails } =
     useGetSpecificUser(user?.id as string);
   console.log(userDetails);
+  const { data: qhpDetails, isLoading: isFetchingQhpDetails } =
+    useGetQhpDetails(user?.id as string);
+  console.log(qhpDetails);
+
   const { data: savedContents, isLoading: isFetchingSavedContents } =
     useGetSavedContents(user?.id as string);
   console.log(savedContents);
@@ -50,10 +55,105 @@ const Profile = () => {
         </div>
         <div>
           {user && user.role === 'qha' && (
-            <div>
+            <>
               <Heading>Professional Details</Heading>
-              <Card></Card>
-            </div>
+
+              <div className='flex items-center justify-center'>
+                {qhpDetails && (
+                  <Card className='max-w-[600px]'>
+                    <CardHeader>
+                      <CardTitle>Details regarding professional life</CardTitle>
+                    </CardHeader>
+                    <CardContent className='flex flex-col gap-3'>
+                      <div className='flex justify-between'>
+                        <label className='font-medium'>Working License: </label>
+                        <span>{qhpDetails.workingLicense}</span>
+                      </div>
+                      <div className='flex justify-between'>
+                        <label className='font-medium'>Working as: </label>
+                        <span>{qhpDetails.jobTitle}</span>
+                      </div>
+                      <div className='flex justify-between'>
+                        <label className='font-medium'>Working at: </label>
+                        <span>{qhpDetails.employerName}</span>
+                      </div>
+                      <div>
+                        <label className='font-medium'>
+                          Relevant qualifications:
+                        </label>
+                        {qhpDetails.qualifications.length === 0 &&
+                          'No relevant qualifications are provided.'}
+                        {qhpDetails.qualifications.map(
+                          (qualification, index) => (
+                            <p key={index} className='flex'>
+                              <span>
+                                <LucideDot />
+                              </span>
+                              <span>{qualification}</span>
+                            </p>
+                          )
+                        )}
+                      </div>
+                      <div>
+                        <label className='font-medium'>
+                          Relevant experiences:
+                        </label>
+                        {qhpDetails.experiences.length === 0 &&
+                          'No relevant experiences are provided.'}
+                        {qhpDetails.experiences.map((experience, index) => (
+                          <p key={index} className='flex'>
+                            <span>
+                              <LucideDot />
+                            </span>
+                            <span>{experience}</span>
+                          </p>
+                        ))}
+                      </div>
+                      <div>
+                        <label className='font-medium'>
+                          Proficient fields:
+                        </label>
+                        {qhpDetails.proficientFields.length === 0 &&
+                          'No relevant experiences are provided.'}
+                        {qhpDetails.proficientFields.map(
+                          (proficientField, index) => (
+                            <p key={index} className='flex'>
+                              <span>
+                                <LucideDot />
+                              </span>
+                              <span>{proficientField}</span>
+                            </p>
+                          )
+                        )}
+                      </div>
+                      <div>
+                        <label className='font-medium'>
+                          Additional Skills:
+                        </label>
+                        {qhpDetails.additionalSkills.length === 0 &&
+                          'No relevant experiences are provided.'}
+                        {qhpDetails.additionalSkills.map(
+                          (additionalSkill, index) => (
+                            <p key={index} className='flex'>
+                              <span>
+                                <LucideDot />
+                              </span>
+                              <span>{additionalSkill}</span>
+                            </p>
+                          )
+                        )}
+                      </div>
+                      <div>
+                        <label className='font-medium'>Additional info:</label>
+                        <p className='px-7'>
+                          {qhpDetails.additionalInformation}
+                        </p>
+                      </div>
+                    </CardContent>
+                  </Card>
+                )}
+              </div>
+            </>
           )}
         </div>
         <div>
---
+++ src/services/qhpDetails/getQhpDetails.ts
@@ -0,0 +1,12 @@
+import { QhpDetails } from '../../types';
+import { axiosInstance } from '../../constants';
+import { useQuery } from '@tanstack/react-query';
+
+export default function useGetQhpDetails(userId: string) {
+  const response = useQuery<QhpDetails>({
+    queryKey: ['qhpDetails', userId],
+    queryFn: () =>
+      axiosInstance.get(`/qhps/get/${userId}`).then((res) => res.data.data),
+  });
+  return response;
+}
