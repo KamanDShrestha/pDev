09451790a67752cb7f8f78e17e84b37e6ac19351
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-04-09 13:06:56 +0545

    refactor: made changes to not show footer in UserLayout page

--- src/pages/layouts/UserLayout.tsx
+++ src/pages/layouts/UserLayout.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Logo from '../../components/Logo';
 import NavBar from '../..//components/NavBar';
 import { ModeToggle } from '../../components/ThemeToggleButton';
 import Footer from '../../components/Footer';
 
 const UserLayout = () => {
+  const location = useLocation();
   return (
     <div className='flex flex-col min-h-screen overflow-hidden'>
       <div className='flex justify-between p-5 '>
@@ -20,7 +21,7 @@ const UserLayout = () => {
       <div className='relative flex-grow p-5'>
         <Outlet />
       </div>
-      {window.location.pathname === '/home' ? null : <Footer />}
+      {location.pathname === '/home' ? null : <Footer />}
     </div>
   );
 };
